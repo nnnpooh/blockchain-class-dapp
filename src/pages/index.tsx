e18506e7d30fbcad9e8eac0089677c5e8bfafded
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-03-03 18:40:36 +0700

    Fix icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import { type NextPage } from "next";
-
+import { Container } from "@mantine/core";
+import { IconShieldLock } from "@tabler/icons-react";
 const Home: NextPage = () => {
-  return <></>;
+  return (
+    <div className="bg-gray-50 py-4">
+      <Container>
+        <IconShieldLock />
+      </Container>
+    </div>
+  );
 };
 
 export default Home;
