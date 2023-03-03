d6e53adc6ee4b8e30b28247e890d60bad7fdcf12
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-03-03 14:07:16 +0700

    Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,67 @@
+// import { FC } from "react";
 import { type NextPage } from "next";
+// import { Container } from "@mantine/core";
+// import {
+//   IconUser,
+//   IconShieldLock,
+//   IconLink,
+//   IconCurrencyEthereum,
+// } from "@tabler/icons-react";
+import NavBar from "@src/components/layout/Navbar";
+import Body from "@src/components/home/Body";
 
 const Home: NextPage = () => {
-  return <></>;
+  return (
+    <div>
+      <NavBar />
+      <Body />
+    </div>
+    // <div className="bg-gray-50 py-4">
+    //   <Container size="xl">
+    //     <div className="flex items-center justify-between">
+    //       <IconShieldLock
+    //         size={50}
+    //         className="cursor-pointer rounded-full border border-sky-800 p-2 text-sky-800 hover:bg-sky-500 hover:text-white"
+    //       />
+    //       <AccountButton />
+    //     </div>
+    //   </Container>
+    // </div>
+  );
 };
 
 export default Home;
+
+// const AccountButton: FC = () => {
+//   const account = "sdfsdfsdf";
+//   const network = { name: "goerli" };
+//   const balance = 0.1;
+
+//   const isEthereumAvailable = true;
+//   if (!isEthereumAvailable) {
+//     return (
+//       <div className="rounded-md bg-red-300 p-2 text-sm italic text-white">
+//         Please Install MetaMask
+//       </div>
+//     );
+//   }
+
+//   const flexItem =
+//     "flex items-center gap-1 rounded-md bg-gray-400 px-3 py-2 text-sm italic text-white";
+//   return (
+//     <div className="flex gap-2">
+//       <div className={flexItem}>
+//         <IconLink size={18} />
+//         {network.name.toUpperCase()}
+//       </div>
+//       <div className={flexItem}>
+//         <IconUser size={18} />
+//         {account}
+//       </div>
+//       <div className={flexItem}>
+//         <IconCurrencyEthereum size={18} />
+//         {balance}
+//       </div>
+//     </div>
+//   );
+// };
